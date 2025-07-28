1ad2753a229a38d8234199a1c3176c8a1fe720ea
Aryan-040 <singharyan432002@gmail.com>
2025-07-28 19:58:00 +0530

    sidebar_dashboard

--- src/modules/home/views/home-view.tsx
+++ src/modules/home/views/home-view.tsx
@@ -15,8 +15,10 @@ export const HomeView = () => {
   return(
   <div className="flex flex-col p-4 gap-y-4">
   <p> Logged in as {session.user.name}</p>
-  <Button onClick={() => authClient.signOut({
-    fetchOptions: { onSuccess: () => router.push("/sign-in")}})}>
+  <Button
+    className="bg-green-600 hover:bg-green-700 text-white border border-green-700"
+    onClick={() => authClient.signOut({
+      fetchOptions: { onSuccess: () => router.push("/sign-in")}})}>
     Sign out
   </Button>
 </div>
